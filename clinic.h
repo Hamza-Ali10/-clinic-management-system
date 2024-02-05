8ca00fb87e40b94eb39a3fd262ed25e66fecd93e
Hamza Ali <108686725+Hamza-Ali10@users.noreply.github.com>
2024-02-05 23:14:23 +0200

    Add files via upload

---
+++ clinic.h
@@ -0,0 +1,12 @@
+// Function prototypes
+void adminMode();
+void userMode();
+int isIdExists(int id);
+void addPatient();
+void editPatient();
+void reserveSlot();
+void cancelReservation();
+void viewPatientRecord();
+void viewTodayReservations();
+void printAvailableSlots();
+void initializeSlots();
